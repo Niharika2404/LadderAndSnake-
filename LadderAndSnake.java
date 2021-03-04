c32ae55991510a3e76dc5122db4073aec7fbbc2e
Niharika2404 <niharikang24@gmail.com>
2021-03-04 10:34:58 +0530

    Dice rolled

--- LadderAndSnake.java
+++ LadderAndSnake.java
@@ -6,5 +6,8 @@ public class LadderAndSnake {
 		public static void main(String[] args) {
 			LadderAndSnake.Player();
 			int position = 0;
+			double dieCheck = (Math.floor(Math.random() * 10) % 5) + 1;
+			System.out.println("Rolled diced number is: " + dieCheck);
+
 		}
 }
