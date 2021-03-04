9c02a994e9fd2c5a452fd9c571603960ad62fab3
Niharika2404 <niharikang24@gmail.com>
2021-03-04 10:21:33 +0530

    Player is in position 0

---
+++ LadderAndSnake.java
@@ -0,0 +1,10 @@
+public class LadderAndSnake {
+		public static void Player() {
+			LadderAndSnake p1 = new LadderAndSnake();
+		}
+
+		public static void main(String[] args) {
+			LadderAndSnake.Player();
+			int position = 0;
+		}
+}
