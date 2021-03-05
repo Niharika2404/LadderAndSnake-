a7040d06a98bb1a8be228df0a3fb94a9f19eed3a
Niharika2404 <niharikang24@gmail.com>
2021-03-05 10:02:24 +0530

    options checked by the player

--- LadderAndSnake.java
+++ LadderAndSnake.java
@@ -6,8 +6,24 @@ public class LadderAndSnake {
 		public static void main(String[] args) {
 			LadderAndSnake.Player();
 			int position = 0;
+			int N0_PLAY = 1;
+			int LADDER = 2;
+			int SNAKE = 3;
+
 			double dieCheck = (Math.floor(Math.random() * 10) % 5) + 1;
-			System.out.println("Rolled diced number is: " + dieCheck);
+					switch (dieCheck) {
+							case 1:
+								position = 0;
+								break;
+							case 2:
+								position = position++ + dieCheck;
+								break;
+							case 3:
+								position--;
+								break;
+					}
+
+			System.out.println(position);
 
 		}
 }
